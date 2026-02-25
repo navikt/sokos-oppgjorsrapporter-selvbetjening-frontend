a142dde0c4b0befdcfd340198ae35aa72e28b93f
Haimanot Tekie <haimanot.tekie@nav.no>
2026-02-25 07:22:58 +0100

    fix(TOB-5725): try to fix redirect url after login

--- src/middleware/urls.ts
+++ src/middleware/urls.ts
@@ -1,1 +1,10 @@
-export const loginUrl = `/oppgjorsrapporter/oauth2/login?redirect=`;
+import { getEnvironment } from '@src/utils/server/urls.ts';
+
+const REDIRECT_ORIGIN = {
+  local: 'http://localhost:4321',
+  development: 'https://www.ekstern.dev.nav.no',
+  production: 'https://www.nav.no',
+};
+
+export const redirectOrigin = REDIRECT_ORIGIN[getEnvironment()];
+export const loginUrl = `/oppgjorsrapporter/oauth2/login?redirect=${redirectOrigin}/oppgjorsrapporter`;
