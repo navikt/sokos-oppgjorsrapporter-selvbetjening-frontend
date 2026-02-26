e7cd532b6cd086bee9f5ec5aab5e76b1d4a5d816
Harald Meland <harald.roisland.meland@nav.no>
2026-02-26 10:53:33 +0100

    Prøver å bruke zod parsing i innhold.ts for å sikre at id + type ser riktige ut - men testene feiler nå på en import

--- src/pages/rapport/[rapportId]/innhold.ts
+++ src/pages/rapport/[rapportId]/innhold.ts
@@ -2,10 +2,38 @@ import type { APIRoute } from 'astro';
 import { oppgjorsrapporterApiUrl } from '@utils/server/urls.ts';
 import logger from '@utils/logger.ts';
 import { exchangeCitizenToken } from '@utils/server/token.ts';
+import { RapportFormat, RapportId } from '@src/schemas/types';
+
+const formatMimeType: Record<RapportFormat, string> = {
+  csv: 'text/csv',
+  pdf: 'application/pdf',
+};
 
 export const GET: APIRoute = async ({ url, params, locals }) => {
-  const id = Number(params.rapportId);
-  const type = url.searchParams.get('type');
+  const paramId = params.rapportId;
+  const paramType = url.searchParams.get('type');
+
+  if (paramId === undefined || paramType === null) {
+    return new Response(
+      JSON.stringify({ error: 'Mangler rapport rapportId eller type' }),
+      { status: 400 },
+    );
+  }
+
+  const parsedId = RapportId.safeParse(paramId);
+  const parsedType = RapportFormat.safeParse(paramType);
+
+  if (!(parsedId.success && parsedType.success)) {
+    return new Response(
+      JSON.stringify({ error: 'Ugyldig rapportId eller type' }),
+      {
+        status: 400,
+      },
+    );
+  }
+
+  const id = parsedId.data;
+  const type = parsedType.data;
 
   const citizenToken = locals.token;
   if (!citizenToken) {
@@ -16,21 +44,8 @@ export const GET: APIRoute = async ({ url, params, locals }) => {
 
   const tokenXToken = await exchangeCitizenToken(citizenToken);
 
-  if (!id || !type) {
-    return new Response(
-      JSON.stringify({ error: 'Mangler rapport rapportId eller type' }),
-      { status: 400 },
-    );
-  }
-
-  if (type !== 'pdf' && type !== 'csv') {
-    return new Response(JSON.stringify({ error: 'Ugyldig rapport type' }), {
-      status: 400,
-    });
-  }
-
   const backendUrl = `${oppgjorsrapporterApiUrl}/${id}/innhold`;
-  const accept = type === 'pdf' ? 'application/pdf' : 'text/csv';
+  const accept = formatMimeType[type];
 
   try {
     const response = await fetch(backendUrl, {
