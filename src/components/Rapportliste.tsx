197cab465c12d8b64d867c5a13cfb10d1dbb10c8
Christian Klem <christian.klem@nav.no>
2026-08-21 10:56:52 +0200

    feat(TOB-6490): Fjern advarsel om doble nedlastninger

--- src/components/Rapportliste.tsx
+++ src/components/Rapportliste.tsx
@@ -1,7 +1,7 @@
 import { type RapportId, RapportMedNedlastningsinfo } from '@src/schemas/types';
 import { text } from '@src/language/text';
 import RapportKort from '@src/components/RapportKort';
-import { BodyLong, Heading, LocalAlert, VStack } from '@navikt/ds-react';
+import { BodyLong, Heading, VStack } from '@navikt/ds-react';
 import { useEffect, useState } from 'react';
 import { setParams } from '@navikt/nav-dekoratoren-moduler';
 
@@ -30,36 +30,6 @@ export default function Rapportliste({ rapportliste }: RapportlisteProps) {
           {text.orgNrLabel}: {rapportliste.orgnr}
         </BodyLong>
       </VStack>
-      {(rapportliste.type === 'ref-arbg' ||
-        rapportliste.type === 'trekk-kred') && (
-        <LocalAlert status="warning">
-          <LocalAlert.Header>
-            <LocalAlert.Title>OBS, unngå doble nedlastinger!</LocalAlert.Title>
-          </LocalAlert.Header>
-          <LocalAlert.Content>
-            <BodyLong>
-              Nav har nå begynt å sende ut oppgjørsrapporter (tidligere kalt K27
-              og T14) via vår nye Altinn 3-baserte løsning.
-            </BodyLong>
-            <BodyLong>
-              For å imøtekomme de som trenger litt tid til å tilpasse sine
-              rutiner, vil vi også{' '}
-              <b>
-                {rapportliste.type === 'ref-arbg'
-                  ? 'frem til 15. juni 2026'
-                  : 'ut mai 2026'}
-              </b>{' '}
-              fortsette å sende tilsvarende rapporter fra den gamle løsningen.
-            </BodyLong>
-            <BodyLong>
-              Rapportene fra ny og gammel løsning inneholder samme informasjon
-              og svarer til samme utbetaling, men vil ha forskjellig tittel i
-              Altinn-innboksen. Du trenger derfor{' '}
-              <b>kun å laste ned én av dem</b>.
-            </BodyLong>
-          </LocalAlert.Content>
-        </LocalAlert>
-      )}
       {rapportliste.rapporter.map((rapport) => (
         <RapportKort
           rapportMetadata={rapport}
